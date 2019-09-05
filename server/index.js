a1b83ca0f6ab7c86bcd6d5e9527f0f76aa00c0a9
Tramy Nguyen <tramynn2@gmail.com>
2019-09-05 14:06:32 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+
+const app = express();
+
+const { SERVER_PORT, CONNECTING_STRING } = process.env;
+// execute some logic when the promise is fulfilled, so chain a .then to it
+// capture db instance in the first parameter
+// set dbInstance onto app and have function return app.set
+massive(CONNECTING_STRING)
+  .then(dbInstance => {
+    app.set("db", dbInstance);
+  })
+  .catch(err => console.log(err));
+
+app.use(express.json());
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on ${SERVER_PORT}`);
+});
